b16b432ef5f7470c6e8501b72a5c6c2e347de4e8
Emma Bowden <97846259+embowden@users.noreply.github.com>
2022-03-24 21:39:36 +0000

    complete draft of options page

--- src/components/CheckActiviy/CheckActiviy.jsx
+++ src/components/CheckActiviy/CheckActiviy.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./check-activiy.scss";
 
 const CheckActiviy = () => {
-  return <div>CheckActiviy</div>;
+  return (
+    <section className="options">
+      <article className="options__container">
+        <Link to="/options/timer" className="options__timer">
+          TIMER
+        </Link>
+        <Link to="/options/manual" className="options__manual">
+          MANUALLY ENTER
+        </Link>
+      </article>
+    </section>
+  );
 };
 
 export default CheckActiviy;
