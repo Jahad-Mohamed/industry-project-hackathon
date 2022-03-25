346577f4e5e2fe17036f63eb0ac68a285f3b0f14
Emma Bowden <97846259+embowden@users.noreply.github.com>
2022-03-25 12:55:01 +0000

    finish second page

--- src/components/CheckActiviy/CheckActiviy.jsx
+++ src/components/CheckActiviy/CheckActiviy.jsx
@@ -1,30 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./check-activiy.scss";
+import clock from "../../assets/icons/whiteClock.svg";
 import edit from "../../assets/icons/edit-icon.svg";
-import arrow from "../../assets/icons/arrow.svg";
+import blueEdit from "../../assets/icons/blueEdit.svg";
+import arrow from "../../assets/icons/whiteArrow2.svg";
+import blueArrow from "../../assets/icons/blueArrow.svg";
+import shower from "../../assets/icons/shower.svg";
+import Header from "../Header/Header";
 
 const CheckActiviy = () => {
   return (
-    <section className="options">
-      <article className="options__container">
-        <h3 className="options__header">Today's shower log</h3>
-        <div className="options__timer">
-          <Link to="/options/timer" className="options__button">
-            <img className="options__icon" src={edit} alt="" />
-            <p className="options__text">Track your time</p>
-            <img className="options__icon" src={arrow} alt="" />
-          </Link>
-        </div>
-        <div className="options__manual">
-          <Link to="/options/manual" className="options__button">
-            <img className="options__icon" src={edit} alt="" />
-            <p className="options__text">Enter Manually</p>
-            <img className="options__icon" src={arrow} alt="" />
-          </Link>
-        </div>
-      </article>
-    </section>
+    <>
+      <Header title="Track Shower" />
+      <section className="options">
+        <section className="options__container">
+          <div className="options__image-container">
+            <img className="options__image" src={shower} alt="" />
+          </div>
+          <h1 className="options__text">
+            Hi Daffy, how would you like to log your shower today?
+          </h1>
+          <div className="options__buttons">
+            <Link className="options__timer" to="/options/timer">
+              <img className="options__icon" src={clock} alt="" /> Timer
+              <img className="options__icon-arrow" src={arrow} alt="" />
+            </Link>
+            <Link className="options__manual" to="/options/manual">
+              <img className="options__icon" src={blueEdit} alt="" /> Enter
+              manually
+              <img className="options__icon-arrow" src={blueArrow} alt="" />
+            </Link>
+          </div>
+        </section>
+      </section>
+    </>
   );
 };
 
