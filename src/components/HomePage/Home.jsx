a376ce6d2056a7228e14ab836abec78fbc0a0fa4
Emma Bowden <97846259+embowden@users.noreply.github.com>
2022-03-25 09:06:55 +0000

    update header

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from "../Header/Header";
 import "./home.scss";
 
 const Home = () => {
   return (
-    <section className="home">
-      <article className="home__container">
-        <h3 className="home__header">March</h3>
-        <div className="home__money">Money</div>
-        <div className="home__water">Water</div>
-        <div className="home__water">Time</div>
-        <Link to="/options" className="home__options">
-          Options
-        </Link>
-      </article>
-    </section>
+    <>
+      <Header title="Overview"/>
+      <section className="home">
+        <article className="home__container">
+          <h3 className="home__header">March</h3>
+          <div className="home__money">Money</div>
+          <div className="home__water">Water</div>
+          <div className="home__water">Time</div>
+          <Link to="/options" className="home__options">
+            Options
+          </Link>
+        </article>
+      </section>
+    </>
   );
 };
 
