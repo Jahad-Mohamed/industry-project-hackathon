46fe3c49df3d5bc72b1f303f93eef34df9fe1218
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-03-25 13:17:48 +0000

    reult

--- src/components/TimerResults/TimerResults.jsx
+++ src/components/TimerResults/TimerResults.jsx
@@ -5,51 +5,55 @@ import pounds from "../../assets/icons/pounds.svg";
 import droplet from "../../assets/icons/droplet.svg";
 import bulb from "../../assets/icons/light-bulb.svg";
 import fire from "../../assets/icons/fire.svg";
+import Header from "../Header/Header";
 
 const TimerResults = () => {
   return (
-    <div className="results__container">
-      <h1 className="results__title">Header</h1>
-      <div className="results__timer-container-main">
-        <div className="results__timer-container">
-          <div className="results__timer">
-            <div className="results__timer-top">
-              <img src={timer} alt="timer" />
-              <div className="results__time"> 5:02</div>
-            </div>
-            <div className="results__best">
-              <img src={fire} alt="fire" />
-              <div className="results__timer-top">New Best!</div>
+    <>
+      <Header title="Your shower time" />
+      <div className="results__container">
+        {/* <h1 className="results__title">Header</h1> */}
+        <div className="results__timer-container-main">
+          <div className="results__timer-container">
+            <div className="results__timer">
+              <div className="results__timer-top">
+                <img src={timer} alt="timer" className="icon__timer" />
+                <div className="results__time"> 5:02</div>
+              </div>
+              <div className="results__best">
+                <img src={fire} alt="fire" />
+                <div className="results__timer-top">New Best!</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="results__comment">
-        Awesome! You're on fire in saving water
-      </div>
-      <div className="results__saving-container">
-        <div className="results__saving">
-          <img src={pounds} alt="pounds" />
-          <div className="results__saving-figures"> 20 £ </div>
+        <div className="results__comment">
+          Awesome! You're on fire in saving water
         </div>
-        <div className="results__saving">
-          <img src={droplet} alt="droplet" />
-          <div className="results__saving-figures"> 20L </div>
+        <div className="results__saving-container">
+          <div className="results__saving">
+            <img src={pounds} alt="pounds" className="icon__pounds" />
+            <div className="results__saving-figures"> 20 £ </div>
+          </div>
+          <div className="results__saving">
+            <img src={droplet} alt="droplet" />
+            <div className="results__saving-figures"> 20L </div>
+          </div>
         </div>
-      </div>
-      <div className="results__info-container">
-        <img src={bulb} alt="light-bulb" />
-        <div className="results__info">
-          <h3 className="results__info-title">Did you know?</h3>
-          <p className="results__info-text">
-            Reducing your shower time by 2 minutes will save you £25 a year and
-            5000 litres of water
-          </p>
+        <div className="results__info-container">
+          <img src={bulb} alt="light-bulb" />
+          <div className="results__info">
+            <h3 className="results__info-title">Did you know?</h3>
+            <p className="results__info-text">
+              Reducing your shower time by 2 minutes will save you £25 a year
+              and 5000 litres of water
+            </p>
+          </div>
         </div>
+        <div className="results__bottom-space"></div>
       </div>
-      <div className="results__bottom-space"></div>
-    </div>
+    </>
   );
 };
 
